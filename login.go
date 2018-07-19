341a3e09e10aafbf223b223c8a71dee5f0dbed8a
silvio io <silvio@silvio.io>
2018-07-19 19:08:26 +0200

    hardcoded scrypt salt and parameter

--- login.go
+++ login.go
@@ -8,7 +8,7 @@ import "golang.org/x/crypto/scrypt"
 // LoginKey returns a 32 byte scrypt derived key.
 func LoginKey(password []byte) *[32]byte {
 	key := [32]byte{}
-	derKey, err := scrypt.Key([]byte(password), scryptSalt, scryptN, scryptR, scryptP, 32)
+	derKey, err := scrypt.Key([]byte(password), []byte{0xd0, 0xdf, 0x83, 0xc0, 0x5d, 0x53, 0x87, 0xb9}, 1<<15, 8, 1, 32)
 	if err != nil {
 		panic(err)
 	}
